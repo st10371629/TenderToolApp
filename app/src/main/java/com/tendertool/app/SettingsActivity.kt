eac4e0b01bf214d9b8acb48cea480633abe2c299
Fathima Shariff <128419928+st10371629@users.noreply.github.com>
2025-09-24 18:33:22 +0200

    - added top tool bar fragment and implemented it on both the settings and discover page. - adjusted padding on some pages - made notification overlay - overlay appears when the bell icon is tapped - created some mock notification data

--- app/src/main/java/com/tendertool/app/SettingsActivity.kt
+++ app/src/main/java/com/tendertool/app/SettingsActivity.kt
@@ -1,13 +1,18 @@
 package com.tendertool.app
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.tendertool.app.src.NavBar
+import com.tendertool.app.src.TopBarFragment
 
 class SettingsActivity : BaseActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        // Attach TopBarFragment to the container
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.topBarContainer, TopBarFragment())
+            .commit()
 
         // attach nav bar listeners
         NavBar.LoadNav(this)
